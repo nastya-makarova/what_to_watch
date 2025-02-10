1c6aa66b30357a4282a1d2f425e7e26b86f42f09
Nastya Makarova <a.o.konovalova@gmail.com>
2025-02-10 22:41:12 +0400

    Added sqlalchemy to connect to db.

--- opinions_app.py
+++ opinions_app.py
@@ -1,11 +1,22 @@
 from flask import Flask
+# Импортируем класс для работы с ORM:
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# Подключаем БД SQLite:
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+# Создаём экземпляр SQLAlchemy и в качестве параметра
+# передаём в него экземпляр приложения Flask:
+db = SQLAlchemy(app)
 
 
 @app.route('/')
 def index_view():
     return 'Совсем скоро тут будет случайное мнение о фильме!'
 
+
 if __name__ == '__main__':
     app.run()
+
+# print(app.config)
